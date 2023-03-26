bb9c8ffd850c8d95dc8ed9a503771a9697e106cc
guo <18213857028@163.com>
2023-03-27 01:52:15 +0800

    lab1 all

--- lab1-hybrid-image/gui/.history/fft_20230325185725.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
-
-img = cv2.imread('res.bunny', 0)
-
--- lab1-hybrid-image/gui/.history/fft_20230325185749.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-from matplotlib import pyplot as plt
-
-
-img = cv2.imread('res.bunny', 0)
-fft = np.fft.fft2(img)
-fftshift = np.fft.fftshift(fft)
